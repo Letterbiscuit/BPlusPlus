f5cc225038f50f7976e0822e7402cef63ce8690c
Letterbiscuit <cmcculloch99@gmail.com>
2015-07-18 14:17:48 +0100

    new file:   Makefile 	new file:   bpp.c

---
+++ src/bpp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>//For terminal and file IO
+#include <stdint.h>//Because uint8_t makes purpose more obvious than char in a memory block
+#include <unistd.h>//System calls to gcc
+#include <stdlib.h>//For malloc and realloc
+
+
+
+int main(int argc, char *argv[]){
+
+
+
+	return 0;
+}
